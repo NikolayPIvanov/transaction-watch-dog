9ebb697274faba790f6dcb93909b503bc0395840
nikolay.ivanov@scalefocus.com <nikolay.ivanov@scalefocus.com>
2022-04-16 09:15:30 +0300

    Removing buisness logic from shared rule schema

--- shared/models/ruleset-schema.js
+++ shared/models/ruleset-schema.js
@@ -30,63 +30,6 @@ const ruleSetSchema = new Schema({
     ]
 });
 
-const fromToFilter = (rules, tx) => {
-    const fromToRules = rules.filter(r => r.from || r.to)
-    const matchingFilter = fromToRules.find(r => r.from === tx.from || r.to === tx.to)
-    return matchingFilter;
-}
-
-const thresholdFilter = (lower, upper, value) => {
-    let match = false
-    if (lower !== undefined && upper !== undefined) {
-        match = lower <= value && value <= upper
-    }
-
-    if (lower !== undefined && !match) {
-        match = lower <= value
-    }
-
-    if (upper !== undefined && !match) {
-        match = value <= upper
-    }
-
-    return match;
-}
-
-const valueFilter = (rules, tx) => {
-    const valueRules = rules.filter(r => r.lowerValueThreshold !== undefined || r.upperValueThreshold !== undefined)
-    const threshold = (rule, tx) => {
-        const [lower, upper] = [rule.lowerValueThreshold, rule.upperValueThreshold]
-        const value = Number(tx.value)
-        const match = thresholdFilter(lower, upper, value)
-        return match ? rule : null
-    }
-
-    const matchingFilter = valueRules.find(r => threshold(r, tx))
-    return matchingFilter;
-}
-
-const gasFilter = (rules, tx) => {
-    const valueRules = rules.filter(r => r.lowerGasThreshold !== undefined || r.upperGasThreshold !== undefined)
-    const threshold = (rule, tx) => {
-        const [lower, upper] = [rule.lowerGasThreshold, rule.upperGasThreshold]
-        const value = Number(tx.gasPrice)
-        const match = thresholdFilter(lower, upper, value)
-        return match ? rule : null
-    }
-
-    const matchingFilter = valueRules.find(r => threshold(r, tx))
-    return matchingFilter;
-}
-
-const filterTransaction = function (tx) {
-    const matchingRules = []
-    matchingRules.push(fromToFilter(this.rules, tx))
-    matchingRules.push(valueFilter(this.rules, tx))
-    matchingRules.push(gasFilter(this.rules, tx))
-    return matchingRules.filter(r => r);
-}
-
 module.exports = {
     ruleSetSchema
 }
