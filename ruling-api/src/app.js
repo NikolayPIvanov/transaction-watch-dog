6c964a446578fd50db4e42b12299db2453069555
nikolay.ivanov@scalefocus.com <nikolay.ivanov@scalefocus.com>
2022-04-16 06:33:13 +0300

    Adding http logging with morgan

--- ruling-api/src/app.js
+++ ruling-api/src/app.js
@@ -1,10 +1,18 @@
 const express = require('express');
 const cors = require('cors')
+const helmet = require("helmet");
+const httpLogger = require('./log/httpLogger')
 
 const app = express();
 
-app.use(cors()); // Enable All CORS 
+app.use(cors()); // Enable All CORS
 
-app.use(express.json());
+app.use(helmet()); // Adding Security Headers
+
+app.use(express.json()); // JSON parsing
+
+// app.use(expressWinston.logger(logger)); // Request logging
+
+app.use(httpLogger)
 
 module.exports = app;
